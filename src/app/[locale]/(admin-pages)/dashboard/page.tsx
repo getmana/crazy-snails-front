cd5a91bb2c319cc801120c42e4cf7e21602f43fc
Anastasia Hetman <a.getman.kh@gmail.com>
2025-10-20 18:13:39 +0300

    Fix Dashboard links.

--- src/app/[locale]/(admin-pages)/dashboard/page.tsx
+++ src/app/[locale]/(admin-pages)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+import { Heading } from '@/components';
 import { Locale } from '@/i18n-config';
 import { getDictionary } from '@/utils';
 
@@ -10,14 +11,14 @@ export default async function Dashboard(props: { params: Promise<{ locale: Local
 
     return (
         <div className="section flex w-full flex-col items-center px-8">
-            <h1>{title.dashboard}</h1>
+            <Heading heading={title.dashboard} className="heading-3" headingTag="h1" />
             <div className="w-full bg-white px-8 py-12 md:w-2xl">
                 <ul className="list-disc">
                     <li>
-                        <Link href="/dashboard/create-album">{title.createAlbum}</Link>
+                        <Link href={`/${locale}/dashboard/create-album`}>{title.createAlbum}</Link>
                     </li>
                     <li>
-                        <Link href="/dashboard/edit-user">{title.editUser}</Link>
+                        <Link href={`/${locale}/dashboard/edit-user`}>{title.editUser}</Link>
                     </li>
                 </ul>
             </div>
